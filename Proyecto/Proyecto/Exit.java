87632b95bdb4efda104c7d34726379bbef6a96c9
CastilloKaren <karencastillo085@gmail.com>
2015-11-11 23:58:09 -0600

    Limites de los objetos
    se definieron algunas condiciones para los objetos en el entorno

---
+++ Proyecto/Proyecto/Exit.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Exit here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Exit extends Actor
+{
+    /**
+     * Act - do whatever the Exit wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
